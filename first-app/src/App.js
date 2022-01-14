e5ed297e425d7421dca8f28cf35577afde476a37
yijoon009 <74906042+yijoon009@users.noreply.github.com>
2022-01-14 22:22:15 +0900

    useEffect

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,10 +1,33 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useState, useEffect } from 'react';
+
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState('');
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (e) => setKeyword(e.target.value);
+  console.log('i run all the time');
+  // const iRunOnlyOnce = () => {
+  //   console.log('i run only once.');
+  // };
+  // useEffect(iRunOnlyOnce, []);
+  useEffect(() => {
+    console.log('CALL THE API....');
+  }, []);
+  useEffect(() => {
+    if (keyword !== '' && keyword.length > 4) {
+      console.log('SEARCH FOR', keyword);
+    }
+  }, [keyword]);
   return (
     <div>
-      <h1 className={styles.title}>Welcome Back!!!</h1>
-      <Button text={"Continue"} />
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click me!</button>
     </div>
   );
 }
