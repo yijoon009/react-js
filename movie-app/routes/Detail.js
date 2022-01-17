0709c9ba5df21499873c47919a027eb6fbb7ad96
yijoon009 <74906042+yijoon009@users.noreply.github.com>
2022-01-17 22:27:45 +0900

    Add files via upload

--- movie-app/routes/Detail.js
+++ movie-app/routes/Detail.js
@@ -1,5 +1,30 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 function Detail() {
-  return <h1>Detail</h1>;
+  const [loading, setLoading] = useState(true);
+  const [movieD, setMovieD] = useState([]);
+  const { id } = useParams();
+  // console.log(id);
+  const getMovie = async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+    // console.log(json);
+    setMovieD(json.data.movie);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMovie();
+  }, []);
+
+  return <div>{loading ? <h2>Detail Loading...</h2> : (
+    <div>
+      {movieD.map((movie) => )}
+
+    </div>
+
+  )}</div>;
 }
 
 export default Detail;
--- movie-app/routes/Home.js
+++ movie-app/routes/Home.js
@@ -33,6 +33,7 @@ function Home() {
         <div>
           {movies.map((movie) => (
             <Movie
+              id={movie.id}
               key={movie.id}
               coverImg={movie.medium_cover_image}
               title={movie.title}
