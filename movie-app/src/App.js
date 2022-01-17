ddd5d46fae7deb3560cc5fea2b92d82546d105ab
yijoon009 <74906042+yijoon009@users.noreply.github.com>
2022-01-17 21:09:12 +0900

    Switch Route

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,8 +1,20 @@
-import { useState, useEffect } from "react";
-import Movie from "./components/Movie";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
 
 function App() {
-  return null;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/movie">
+          <Detail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
